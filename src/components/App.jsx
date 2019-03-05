b8dec81af4a06416d272f05616f9e7ff389f490d
stellaliang21 <stellaliang2014@gmail.com>
2019-03-04 20:46:24 -0800

    implement video title and description in VideoPlayer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import VideoList from "./VideoList.js";
-
+import VideoPlayer from "./VideoPlayer.js"
+//pass props into the Apps function
 var App = (props) => (
   <div>
     <nav className="navbar">
@@ -9,10 +10,10 @@ var App = (props) => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><em><VideoPlayer videos={props.videos}/></em></h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em><VideoList videos={props.videos}/></em> view goes here</h5></div>
+        <div><h5><em><VideoList videos={props.videos}/></em></h5></div>
       </div>
     </div>
   </div>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';  
-// import VideoList from "./VideoList";
-// import VideoListEntry from "./VideoListEntry";
-// import VideoPlayer from "./VideoPlayer";
-// import Search from "./Search";
+// importing the exampleVideoData from the exampleVideoData file
 import exampleVideoData from './data/exampleVideoData.js';
 
+//Rendering the 'App' componet to the DOM
 ReactDOM.render(<App videos={exampleVideoData}/>, document.getElementById('app'));
 
-//'helllo'
